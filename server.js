53f686596dfa14c6f6fa9c3dce8a410e9ea0cb0c
Anzel Capparelli <anzelcapparelli@gmail.com>
2021-02-23 18:39:06 -0800

    initial MERN boiler-plate commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+
+// ====================================================================================================
+
+// const routes = require("./routes");
+// ^^^ exports all routes into a index.js file, which is what's sent into this file
+
+// app.use(routes);
+// ^^^ activate once you have routes folder!
+
+// ====================================================================================================
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
+
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
