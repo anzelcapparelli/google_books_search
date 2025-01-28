5253b32f9da98e26e954e2568fed4ff58802381a
Anzel Capparelli <anzelcapparelli@gmail.com>
2025-01-28 00:14:57 -0800

    updated code to restore local functionality after npm audit fix

--- server.js
+++ server.js
@@ -31,8 +31,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1/googlebooks",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
   }
 );
 
